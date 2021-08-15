9ca208719d00a8afb07b0a15b7734561405ccf9c
papasanimohansrinivas <papasani.mohansrinivas@gmail.com>
2021-08-15 16:24:41 +0530

    test_tf_records

--- uflow/data_conversion_scripts/convert_flying_chairs_to_tfrecords.py
+++ uflow/data_conversion_scripts/convert_flying_chairs_to_tfrecords.py
@@ -48,15 +48,27 @@ def convert_dataset():
   #   tf.io.gfile.mkdir(test_dir)
 
   # Directory with images.
-  images = sorted(tf.io.gfile.glob(FLAGS.data_dir + '/*.ppm'))
-  # flow_list = sorted(tf.io.gfile.glob(FLAGS.data_dir + '/*.flo'))
-  assert len(images) // 2 == len(flow_list)
   image_list = []
-  for i in range(len(flow_list)):
-    im1 = images[2 * i]
-    im2 = images[2 * i + 1]
-    image_list.append((im1, im2))
-  assert len(image_list) == len(flow_list)
+  for folder_name in FLAGS.data_dir:
+    # files_list  = sorted(os.listdir(os.path.join(input_main_folder_name,folder_name)),key= lambda x : int(x.split("_")[3]))
+    files_list  = sorted(tf.io.gfile.glob(FLAGS.data_dir + '/'+folder_name),key= lambda x : int(x.split("_")[3]))
+    for index in range(len(files_list)):
+      if (index!=len(files_list)-1):
+        if files_list[index].split("_")[3]!=files_list[index+1].split("_")[3]:
+          # print(files_list[index],files_list[index+1])
+          # total_pairs+=1
+          moving_file_name = os.path.join(input_main_folder_name,folder_name,files_list[index])
+          static_file_name = os.path.join(input_main_folder_name,folder_name,files_list[index+1])
+          image_list.append((moving_file_name,static_file_name))
+  # images = sorted(tf.io.gfile.glob(FLAGS.data_dir + '/*.ppm'))
+  # flow_list = sorted(tf.io.gfile.glob(FLAGS.data_dir + '/*.flo'))
+
+  
+  # for i in range(len(flow_list)):
+  #   im1 = images[2 * i]
+  #   im2 = images[2 * i + 1]
+  #   image_list.append((im1, im2))
+  # assert len(image_list) == len(flow_list)
 
   # Reading ppm and flo can fail on network filesystem, so copy to tmpdir first.
   tmpdir = '/tmp/flying_chairs'
@@ -65,57 +77,58 @@ def convert_dataset():
 
   train_filenames = conversion_utils.generate_sharded_filenames(
       os.path.join(train_dir, 'flying_chairs@{}'.format(FLAGS.num_shards)))
-  test_filenames = conversion_utils.generate_sharded_filenames(
-      os.path.join(test_dir, 'flying_chairs@{}'.format(FLAGS.num_shards)))
+  # test_filenames = conversion_utils.generate_sharded_filenames(
+      # os.path.join(test_dir, 'flying_chairs@{}'.format(FLAGS.num_shards)))
   train_record_writer = tf.io.TFRecordWriter(train_filenames[FLAGS.shard])
-  test_record_writer = tf.io.TFRecordWriter(test_filenames[FLAGS.shard])
+  # test_record_writer = tf.io.TFRecordWriter(test_filenames[FLAGS.shard])
   total = len(image_list)
   images_per_shard = total // FLAGS.num_shards
   start = images_per_shard * FLAGS.shard
-  filepath = FLAGS.train_split_file
-  with open(filepath, mode='r') as f:
-    train_val = f.readlines()
-    train_val = [int(x.strip()) for x in train_val]
+  # filepath = FLAGS.train_split_file
+  # with open(filepath, mode='r') as f:
+  #   train_val = f.readlines()
+  #   train_val = [int(x.strip()) for x in train_val]
   if FLAGS.shard == FLAGS.num_shards - 1:
     end = len(image_list)
   else:
     end = start + images_per_shard
-  assert len(train_val) == len(image_list)
-  assert len(flow_list) == len(train_val)
+  # assert len(train_val) == len(image_list)
+  # assert len(flow_list) == len(train_val)
   image_list = image_list[start:end]
-  train_val = train_val[start:end]
-  flow_list = flow_list[start:end]
+  # train_val = train_val[start:end]
+  # flow_list = flow_list[start:end]
 
   tf.compat.v1.logging.info('Writing %d images per shard', images_per_shard)
   tf.compat.v1.logging.info('Writing range %d to %d of %d total.', start, end,
                             total)
 
-  img1_path = os.path.join(tmpdir, 'img1.ppm')
-  img2_path = os.path.join(tmpdir, 'img2.ppm')
-  flow_path = os.path.join(tmpdir, 'flow.flo')
+  img1_path = os.path.join(tmpdir, 'img1.png')
+  img2_path = os.path.join(tmpdir, 'img2.png')
+  # flow_path = os.path.join(tmpdir, 'flow.flo')
 
-  for i, (images, flow,
-          assignment) in enumerate(zip(image_list, flow_list, train_val)):
+  # for i, (images, flow,
+          # assignment) in enumerate(zip(image_list, flow_list, train_val)):
+  for i, images in enumerate(image_list):
     if os.path.exists(img1_path):
       os.remove(img1_path)
     if os.path.exists(img2_path):
       os.remove(img2_path)
-    if os.path.exists(flow_path):
-      os.remove(flow_path)
+    # if os.path.exists(flow_path):
+      # os.remove(flow_path)
 
     tf.io.gfile.copy(images[0], img1_path)
     tf.io.gfile.copy(images[1], img2_path)
-    tf.io.gfile.copy(flow, flow_path)
+    # tf.io.gfile.copy(flow, flow_path)
 
     image1_data = imageio.imread(img1_path)
     image2_data = imageio.imread(img2_path)
-    flow_data = conversion_utils.read_flow(flow_path)
+    # flow_data = conversion_utils.read_flow(flow_path)
 
     height = image1_data.shape[0]
     width = image1_data.shape[1]
 
-    assert height == image2_data.shape[0] == flow_data.shape[0]
-    assert width == image2_data.shape[1] == flow_data.shape[1]
+    assert height == image2_data.shape[0]
+    assert width == image2_data.shape[1]
 
     example = tf.train.SequenceExample(
         context=tf.train.Features(
@@ -124,8 +137,8 @@ def convert_dataset():
                     conversion_utils.int64_feature(height),
                 'width':
                     conversion_utils.int64_feature(width),
-                'flow_uv':
-                    conversion_utils.bytes_feature(flow_data.tobytes()),
+                # 'flow_uv':
+                    # conversion_utils.bytes_feature(flow_data.tobytes()),
                 'image1_path':
                     conversion_utils.bytes_feature(str.encode(images[0])),
                 'image2_path':
@@ -142,15 +155,15 @@ def convert_dataset():
     if i % 10 == 0:
       tf.compat.v1.logging.info('Writing %d out of %d total.', i,
                                 len(image_list))
-    if assignment == 1:
-      train_record_writer.write(example.SerializeToString())
-    elif assignment == 2:
-      test_record_writer.write(example.SerializeToString())
-    else:
-      assert False, 'There is an error in the chairs_train_val.txt'
+    # if assignment == 1:
+    train_record_writer.write(example.SerializeToString())
+    # elif assignment == 2:
+    #   test_record_writer.write(example.SerializeToString())
+    # else:
+      # assert False, 'There is an error in the chairs_train_val.txt'
 
   train_record_writer.close()
-  test_record_writer.close()
+  # test_record_writer.close()
   tf.compat.v1.logging.info('Saved results to %s', FLAGS.output_dir)
 
 
